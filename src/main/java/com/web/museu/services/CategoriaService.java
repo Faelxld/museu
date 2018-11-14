1b96f5d985c10c3b7ac7bfa013926fd493e91384
rafael <rafaelxld@hotmail.com>
2018-11-14 11:30:13 -0200

    CRUD Categoria na camada de repositories

--- src/main/java/com/web/museu/services/CategoriaService.java
+++ src/main/java/com/web/museu/services/CategoriaService.java
@@ -5,7 +5,8 @@ import com.web.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @Service
 public class CategoriaService {
@@ -18,6 +19,24 @@ public Categoria findOne(Integer id ){
 
     return repository.findOne(id);
 
+}
+
+public List<Categoria> listar(){
+
+        return repository.findAll();
+
+}
+
+public void deletar(Integer id){
+    repository.delete(id);
+}
+
+public void salvar(Categoria categoria){
+    repository.save(categoria);
+}
+
+public void atualizar(Categoria categoria){
+    repository.saveAndFlush(categoria);
 }
 
 }
