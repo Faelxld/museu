a232328c3fe25b0352d0e3abf444f9a1d7e4b0b5
rafael <rafaelxld@hotmail.com>
2018-11-14 19:23:16 -0200

    CRUD Categoria na camada de repositories

--- src/main/java/com/web/museu/services/CategoriaService.java
+++ src/main/java/com/web/museu/services/CategoriaService.java
@@ -1,7 +1,7 @@
 package com.web.museu.services;
 
 import com.web.museu.domain.Categoria;
-import com.web.repositories.CategoriaRepository;
+import com.web.museu.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,17 +15,9 @@ public class CategoriaService {
 public CategoriaRepository repository;
 
 
-public Categoria findOne(Integer id ){
+public Categoria findOne(Integer id ){ return repository.findOne(id); }
 
-    return repository.findOne(id);
-
-}
-
-public List<Categoria> listar(){
-
-        return repository.findAll();
-
-}
+public List<Categoria> listar(){ return repository.findAll(); }
 
 public void deletar(Integer id){
     repository.delete(id);
--- src/main/java/com/web/museu/services/ClasseService.java
+++ src/main/java/com/web/museu/services/ClasseService.java
@@ -1,11 +1,13 @@
 package com.web.museu.services;
 
 
+import com.web.museu.domain.Categoria;
 import com.web.museu.domain.Classe;
 import com.web.museu.repositories.ClasseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +22,18 @@ public Classe findOne(Integer id ){
     return repository.findOne(id);
 
 }
+    public List<Classe> listar(){
+
+        return repository.findAll();
+
+    }
+
+    public void deletar(Integer id){
+        repository.delete(id);
+    }
+
+    public void salvar(Classe classe){  repository.save(classe); }
+
+    public void atualizar(Classe classe){repository.saveAndFlush(classe); }
 
 }
